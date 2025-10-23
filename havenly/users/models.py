0db28d86aa9cc8d979a9e33646bd180107fdb27b
allanXQ <allancjuma@gmail.com>
2025-10-23 10:31:52 +0300

    fix: dev issues
    -  set up custom user model
    - migration conflict
    - reinstalled dependencies

--- havenly/users/models.py
+++ havenly/users/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-import uuid
-from havenly.common.models import BaseKYCModel
+from common.models import BaseKYCModel
 
 
 # Create your models here.
-class BaseUser(AbstractUser):
+class BaseUserModel(AbstractUser):
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True) # to be migrated to a cloud storage later
     language_preference = models.CharField(max_length=10, default='en')
     theme_preference = models.CharField(max_length=10, default='light')
@@ -14,7 +13,7 @@ class BaseUser(AbstractUser):
         return f"{self.first_name} {self.last_name} - {self.user_type}"
 
 class UserKYC(BaseKYCModel):
-    user = models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(BaseUserModel, on_delete=models.CASCADE, related_name='user_kyc')
 
     def __str__(self):
         return f"KYC for {self.user.username} - Verified: {self.verified}"
